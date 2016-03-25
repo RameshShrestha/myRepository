1ed0c1e5ff2619ce94cf0e14b5efaf16f77616d3
Ramesh Shrestha <fx_ra@hotmail.com>
2016-03-25 12:06:12 +0530

    rameshshrestha

---
+++ testAppinGit/WebContent/testappingit/ramesh.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("testappingit.ramesh", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf testappingit.ramesh
+	*/ 
+	getControllerName : function() {
+		return "testappingit.ramesh";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf testappingit.ramesh
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
